23afc50e600be16c6c9e1e15f8f42b1aea7063ff
Benjamin Siegel <bsiegel@upchs.org>
2018-01-09 23:24:06 +0000

    Commit message

---
+++ script.js
@@ -0,0 +1,19 @@
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+function onYouTubeApiLoad() {
+    gapi.client.setApiKey('YOUR_API_KEY_HERE');
+}
+ 
+function search() {
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q:'ADD_QUERY_HERE'
+    });
+    request.execute(onSearchResponse);
+}
+
+function onSearchResponse(response) {
+    console.log(response)
+}
